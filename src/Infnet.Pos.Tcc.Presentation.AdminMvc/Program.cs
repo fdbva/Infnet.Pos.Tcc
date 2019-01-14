7f65a47384a5ff6ddc7240c40441548d9ea69933
Felipe Villela <fdbva@users.noreply.github.com>
2019-01-14 08:51:24 -0200

    DDD projects and presentation in ASP.NET Core MVC (#1)

---
+++ src/Infnet.Pos.Tcc.Presentation.AdminMvc/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Infnet.Pos.Tcc.Presentation.AdminMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
