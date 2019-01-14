9f4d22b3689eb2db76f0f1fedbb31443d54f2d60
Felipe Andrade <villela.felipe@gmail.com>
2019-01-14 08:48:45 -0200

    DDD projects and presentation in ASP.NET Core MVC

---
+++ src/Infnet.Pos.Tcc.Presentation.AdminMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Infnet.Pos.Tcc.Presentation.AdminMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
